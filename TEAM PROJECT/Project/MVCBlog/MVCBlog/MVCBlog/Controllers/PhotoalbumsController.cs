f35ed7d76fe93dbb1290281eae6d4198630743db
Stiliyan Goranov <stiliyangoranov88@gmail.com>
2016-08-30 17:18:14 +0300

    Photoalbum

--- TEAM PROJECT/Project/MVCBlog/MVCBlog/MVCBlog/Controllers/PhotoalbumsController.cs
+++ TEAM PROJECT/Project/MVCBlog/MVCBlog/MVCBlog/Controllers/PhotoalbumsController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MVCBlog.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,65 @@ namespace MVCBlog.Controllers
 {
     public class PhotoalbumsController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Photoalbums
         public ActionResult Index()
         {
+            List<Photoalbum> all = new List<Photoalbum>();
+            all = db.Images.ToList();
+            return View(all);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase file)
+        {
+            Photoalbum IG = new Photoalbum();
+            IG.FileName = file.FileName;
+            IG.ImageSize = file.ContentLength;
+
+            byte[] data = new byte[file.ContentLength];
+            file.InputStream.Read(data, 0, file.ContentLength);
+
+            IG.ImageData = data;
+
+            var model = new Photoalbum
+            {
+                FileName = file.FileName,
+                ImageSize = file.ContentLength,
+                ImageData = data,
+                File = file
+            };
+            db.Images.Add(IG);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photoalbum image = db.Images.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photoalbum image = db.Images.Find(id);
+            db.Images.Remove(image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-}
+}  
